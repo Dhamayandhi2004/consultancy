5d9cca03e0a6426dc899d173aa54e4d8d9696fbc
Dhamayandhi <kavindhamayandhi@gmail.com>
2025-05-22 23:34:01 +0530

    adding file

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://consultancy-ashen.vercel.app'],
+  origin: ['http://localhost:3000', 'https://consultancy-ashen.vercel.app/'],
   credentials: true
 }));
 app.use(express.json());
